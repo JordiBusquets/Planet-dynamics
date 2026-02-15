708fefa2874ac8d80a842d05c38f04c87ed302b0
JordiBusquets <jordi.busquets@gmail.com>
2026-02-15 21:33:40 +0100

    contants

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,9 @@
 # some useful constants
 M_earth = 5.972 * (10.0 ** 24)  # in kg
 R_earth: float = 6.371 * (10.0 ** 6)  # in m
-M_sun = 1.989 * (10.0 ** 30)  # in kg
+M_sun = 1.9885 * (10.0 ** 30)  # in kg
 R_sun = 6.9551 * (10.0 ** 8)  # in m
-D_earth_sun = 1.4714 * (10.0 ** 11)  # in m
+D_earth_sun = 1.496 * (10.0 ** 11)  # mean distance, in m
 D_earth_moon = 3.844 * (10.0 ** 8)  # in m
-V_earth = 30000.0  # in m/s
-G = 6.67408 * (10.0 ** -11.0)  # gravitational constant, in m^3 / (kg * s^2)
+V_earth = 29780.0  # mean orbital velocity in m/s
+G = 6.6743 * (10.0 ** -11.0)  # gravitational constant, in m^3 / (kg * s^2)
