53c74d5a970cc0813cb8920446609807fd122728
JordiBusquets <jordi.busquets@gmail.com>
2026-02-15 12:37:48 +0100

    reorg

---
+++ src/visualization/__init__.py
@@ -0,0 +1,8 @@
+"""Visualization tools for planet dynamics simulation."""
+from .plots import set_up_plot, update_plot, compute_limit
+
+__all__ = [
+    'set_up_plot',
+    'update_plot',
+    'compute_limit',
+]
